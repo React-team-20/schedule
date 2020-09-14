5977e361e615cccf95c6e628be15e7b0c05bbe6a
Eugenia Shibkova <shibkova91@gmail.com>
2020-09-14 23:34:29 +0300

    refactor: refactor ScheduleList component

--- src/components/ScheduleList/ScheduleList.js
+++ src/components/ScheduleList/ScheduleList.js
@@ -1,51 +1,37 @@
-import {Avatar, Divider, List, Tag} from 'antd';
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {setTagColor} from '../../utils';
+import {Divider} from 'antd';
+import {filterDateByMonthAndYear} from '../../utils';
+import ScheduleListItem from './ScheduleListItem';
 
-const getListItem = data => {
-  return (
-    <div className="list-item-wrapper">
-      <Divider className="list-item-divider" orientation="left">
-        September, 2020
-      </Divider>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <div className="tag-wrapper">
-              <Tag className="list-item-tag" color={setTagColor(item.type)}>
-                {item.type
-                  .toUpperCase()
-                  .split('')
-                  .map(i => (i === '-' ? ' ' : i))
-                  .join('')}
-              </Tag>
-            </div>
-            <List.Item.Meta
-              title={<a href="https://ant.design">{item.topic}</a>}
-              description={`${item.date}, ${item.time}`}
-            />
-            <span className="item-organizer">organizer:</span>
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            <a href="#">link</a>
-          </List.Item>
-        )}
-      />
-    </div>
-  );
+const getDivider = (data) => {
+  return <Divider className="list-item-divider" orientation="left">{data}</Divider>  
+};
+
+const getListItem = (key, data) => {
+  return ( 
+    <div key={key} className="list-item-wrapper">
+      {getDivider(key)}
+      {ScheduleListItem(data)}
+    </div>   
+  )    
 };
 
 const ScheduleList = () => {
-  const data = useSelector(state => state.events);
+  const data = useSelector(state => state.events);  
+  const filterData = filterDateByMonthAndYear(data);
+  const listItems = [];
+
+  for (let key in filterData) {  
+    const item = getListItem(key, filterData[key]);
+    listItems.push(item)
+  }
 
   return (
-    <div className="list-wrapper">
-      {getListItem(data)}
-      {getListItem(data)}
-    </div>
-  );
+    <div className="list-wrapper"> 
+      {listItems}  
+    </div>  
+  )
 };
 
 export default ScheduleList;
