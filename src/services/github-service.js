2f6e30d2761a0cbd1b75e622a3eae21e47b1c6ac
Eugenia Shibkova <shibkova91@gmail.com>
2020-09-15 14:39:36 +0300

    refactor: remove unnecessary code

--- src/services/github-service.js
+++
@@ -1,7 +0,0 @@
-export default class GithubService {
-  getUserData = async name => {
-    const res = await fetch(`https://api.github.com/user?q=${name}`);
-    const data = await res.json();
-    return data;
-  };
-}  
