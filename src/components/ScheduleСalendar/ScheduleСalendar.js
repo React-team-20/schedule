0c93c6bc6aa899aaf5b1ba227046d3c8c7a7cddc
Maxim <foba1995@gmail.com>
2020-09-16 21:29:41 +0400

    feat: add overview for day events

--- src/components/ScheduleСalendar/ScheduleСalendar.js
+++ src/components/ScheduleСalendar/ScheduleСalendar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import moment from 'moment';
-import {connect} from 'react-redux';
-import {Calendar, Tag, Badge} from 'antd';
+import {Calendar, Tag, Badge, Drawer} from 'antd';
+import {setTagColor} from '../../utils';
+import ScheduleСalendarDrawer from './ScheduleCalendarDrawer'
 import './schedule-calendar.css';
 
-const connector = connect(state => ({
-  info: state.events,
-}));
 
-const ScheduleСalendar = ({info}) => {
+
+const ScheduleСalendar = ({events}) => {
+  const [drawer, setDrawer] = useState({ visible:false });
+  const [dayEvents, setDayEvents] = useState(0)
+  
+  const showDrawer = (value) => {
+    setDrawer({visible:true})
+    setDayEvents(value)
+  }
+
+  const hiderDrawer = () => {
+    setDrawer({visible:false})
+  }
+
+
   function getListData(value) {
-    const listDatabyMonth = info.filter(
+    const listDatabyYear = events.filter(
+      el => moment(el.dateTime).format('Y') === value.format('Y')
+    );
+    const listDatabyMonth = listDatabyYear.filter(
       el => moment(el.dateTime).format('D') === value.format('D')
     );
     const listDatabyDay = listDatabyMonth.filter(
@@ -22,7 +37,7 @@ const ScheduleСalendar = ({info}) => {
   function dateCellRender(value) {
     const listData = getListData(value);
     return (
-      <div className="events">
+      <div className="events" onClick={()=>{showDrawer(listData)}}>
         <div>
           <Badge
             size="small"
@@ -32,21 +47,24 @@ const ScheduleСalendar = ({info}) => {
           />
           {listData.map(item => (
             <div className="event-box" key={item.id}>
-              <Tag>{item.type}</Tag>
+              <Tag className='tag-type-calendar' color={setTagColor(item.type)}>{item.type}</Tag>
             </div>
           ))}
         </div>
       </div>
     );
   }
-
+const drawerDate = dayEvents ? moment(dayEvents[0].dateTime).format('MMMM Do') : 0;
   return (
     <div className="calendar-container">
       <Calendar
         dateCellRender={dateCellRender}
       />
+      <Drawer title={drawerDate} placement='bottom' closable={false} onClose={hiderDrawer} visible={drawer.visible}>
+          <ScheduleСalendarDrawer dayEvents={dayEvents}/>
+      </Drawer>
     </div>
   );
 };
 
-export default connector(ScheduleСalendar);
+export default ScheduleСalendar;
