e02130c7d08ecedb0e9ca96213d5f13fa47b553d
ThatcheRRR <pavelosipov777@mail.ru>
2020-09-18 18:09:16 +0300

    feat: add feedback form & store

--- src/components/TaskOverview/TaskOverview.js
+++ src/components/TaskOverview/TaskOverview.js
@@ -1,4 +1,4 @@
-import {Modal, Checkbox, Avatar} from 'antd';
+import {Modal, Avatar, Input, Button, Form} from 'antd';
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {hideTaskOverview} from '../../actions';
@@ -9,6 +9,8 @@ const TaskOverview = () => {
   const events = useSelector(state => state.events.events);
   const {isShowTaskOverview, currentEvent} = useSelector(state => state.app);
   const [event, setEvent] = useState(null);
+  const [feedback, setFeedback] = useState('');
+  const [form] = Form.useForm();
 
   useEffect(() => {
     if (isShowTaskOverview && currentEvent) {
@@ -26,6 +28,25 @@ const TaskOverview = () => {
     dispatch(hideTaskOverview());
     setEvent(null);
   };
+
+  const handleType = (e) => {
+    setFeedback(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    form.resetFields();
+    let feedbacksArray;
+    if(localStorage.getItem("feedbacks")) {
+      feedbacksArray = JSON.parse(localStorage.getItem("feedbacks"));
+    } else {
+      feedbacksArray = [];
+    }
+    const feedbackId = new Date().getTime();
+    const newFeedback = {feedbackId, feedback};
+    feedbacksArray.push(newFeedback);
+    localStorage.setItem('feedbacks', JSON.stringify(feedbacksArray));
+  };
+
   return (
     <Modal
       title="Task information"
@@ -34,25 +55,47 @@ const TaskOverview = () => {
       onCancel={handleCancel}
     >
     {event &&
-      <>
-      <h2 className = 'topic'>
-        {event.topic}
-      </h2>
-      <p className = 'organizer'>
-        <span>Organizer:</span>
-        <a href = {event.organizer.htmlUrl} target = '_blank'>
-          <Avatar size = 'small' src = {event.organizer.avatar} />
-          {event.organizer.name}
-        </a>
-      </p>
-      <p><span>Description:</span> {event.description}</p>
-      {event.taskObj.demoUrl && <p><span>Demo:</span> {event.taskObj.demoUrl}</p>}
-      <p><span>Materials:</span>{event.taskObj.materials}</p>
-      {event.place && <p><span>Materials:</span>{event.taskObj.materials}</p>}
-      <p>
-        Allow feedback <Checkbox />
-      </p>
-      </>
+      <div className="overview">
+        <h2 className="topic">
+          {event.topic}
+        </h2>
+        <div className="organizer">
+          <span>Organizer:</span>
+          <a href={event.organizer.htmlUrl} target="_blank">
+            <Avatar
+              size="small"
+              src={event.organizer.avatar}
+            />
+            {event.organizer.name}
+          </a>
+        </div>
+        <div>
+          <span>Description:</span> {event.description}
+        </div>
+        {event.taskObj.demoUrl && <p><span>Demo:</span> {event.taskObj.demoUrl}</p>}
+        <div>
+          <span>Materials:</span>{event.taskObj.materials}
+        </div>
+        {event.place && <div><span>Materials:</span>{event.taskObj.materials}</div>}
+        <div>
+          <Form layout="vertical" id="feedback-form" form={form} onFinish={handleSubmit}>
+            <Form.Item
+              name="feedback"
+              label="Feedback"
+            >
+              <Input.TextArea
+                rows={5}
+                placeholder="Please add your feedback"
+                onChange={handleType}
+                value={feedback} 
+              />
+            </Form.Item>
+            <Button type="primary" form="feedback-form" htmlType="submit">
+              Send feedback
+            </Button>
+          </Form>
+        </div>
+      </div>
     }
     </Modal>
   );
