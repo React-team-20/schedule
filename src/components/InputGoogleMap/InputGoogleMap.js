681000fa01fbeae9f5540c2d416d84d9fae863dd
Vitali Matusevich <vimatu@gmail.com>
2020-09-21 12:02:29 +0300

    feat: add MapsModal to inputInputGoogleMap

--- src/components/InputGoogleMap/InputGoogleMap.js
+++ src/components/InputGoogleMap/InputGoogleMap.js
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Input} from 'antd';
 import {EnvironmentOutlined} from '@ant-design/icons';
 
 import './InputGoogleMap.css';
+import Modal from 'antd/lib/modal/Modal';
 
 const GOOGLE_MAPS_API_KEY = '';
 
-const AddonAfterButton = () => (
-  <Button
-    className="button-center-icon button-no-border"
-    size="small"
-    // onClick={handlerShowMap}
-  >
-    <EnvironmentOutlined />
-  </Button>
-);
-
 const InputGoogleMap = () => {
+  const {visibleModal, setVisibleModal} = useState(false);
+
+  // const handlerShowMap = () => {
+  //   setVisibleModal(({visibleModal}) => !visibleModal);
+  // };
+
+  const AddonAfterButton = () => (
+    <Button
+      className="button-center-icon button-no-border"
+      size="small"
+      // onClick={handlerShowMap}
+    >
+      <EnvironmentOutlined />
+    </Button>
+  );
+
+  const MapsModal = () => (
+    <Modal
+      title="Google maps"
+      visible={visibleModal}
+      // onOk={this.handleOk}
+      // confirmLoading={confirmLoading}
+      // onCancel={this.handleCancel}
+    >
+      <p>ModalText</p>
+    </Modal>
+  );
+
   return (
     <div>
       <Input placeholder="input address" addonAfter={<AddonAfterButton />} />
+      <MapsModal />
     </div>
   );
 };
