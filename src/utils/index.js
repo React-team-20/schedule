a1d314289d5ce7cbe522160be3c933a66ab062aa
Eugenia Shibkova <shibkova91@gmail.com>
2020-09-22 22:21:30 +0300

    feat: add date util functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,8 +8,20 @@ export const dateTimeParse = (dateTime, timezone) => {
   };
 };
 
+export const shortDateByDayParse = (dateTime) => {
+  return moment(dateTime).format('DD');
+};
+
+export const shortDateByDayOfWeekParse = (dateTime) => {
+  return moment(dateTime).format('ddd');
+};
+
 export const dateByMonthAndDayParse = (dateTime) => {
   return moment(dateTime).format('dddd MMMM DD');
+};
+
+export const dateByMonthAndYearParse = (dateTime) => {
+  return moment(dateTime).format('MMMM YYYY');
 };
 
 export const setTagColor = tag => eventsTypes.find(item => item.value === tag).background;
@@ -27,10 +39,6 @@ export const setTagStyle = (tag, eventTypes) => {
   } else {
     return defaultTag;
   }
-};
-
-export const dateByMonthAndYearParse = (dateTime) => {
-  return moment(dateTime).format('MMMM YYYY');
 };
 
 export const isLinkRegExp = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/;
