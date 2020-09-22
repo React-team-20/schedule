bca216ec7387470eb20b637b0c9d438a76027762
Eugenia Shibkova <shibkova91@gmail.com>
2020-09-22 18:11:26 +0300

    refactor: refactor ScheduleListItem component

--- src/components/ScheduleList/ScheduleListItem/ScheduleListItem.js
+++ src/components/ScheduleList/ScheduleListItem/ScheduleListItem.js
@@ -1,39 +1,157 @@
-import {List, Tag} from 'antd';
-import React from 'react';
-import {setTagColor} from '../../../utils';
+import {List, Tag, Button, Divider} from 'antd';
+import {EyeOutlined} from '@ant-design/icons';
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {setHiddenEvents, removeHiddenEvent, showTaskOverview} from '../../../actions';
+import moment from 'moment-timezone';
+
+import EventHideButton from '../../ScheduleTable/EventHideButton';
+
+import {setTagColor, dateByMonthAndYearParse, dateByMonthAndDayParse} from '../../../utils';
 import GithubUserLink from '../../GithubUserLink';
 
-const ScheduleListItem = data => {
+const ScheduleListItem = (data) => {
+  const dispatch = useDispatch();
+  const {hiddenEvents} = useSelector(state => state.events);
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const handlerEventShow = id => {
+    dispatch(removeHiddenEvent(id));
+  };
+
+  const handleClickCapture = (event, id) => {
+    if (event.target.id) return false;
+
+    if (event.shiftKey) {
+      onSelectRow(id, true);
+    } else {
+      onSelectRow(id);
+    }
+  };
+
+  const onSelectRow = (id, isShift = false) => {
+    if (isShift) {      
+      if (selectedRows.includes(id)) {
+        setSelectedRows(selectedRows.filter(i => i !== id));
+      } else {
+        setSelectedRows([...selectedRows, id]);
+      }
+    } else {
+      setSelectedRows(selectedRows[0] === id ? [] : [id]);
+    }
+  };
+
+  const showTaskInfo = id => {
+    dispatch(showTaskOverview(id));
+  };
+
+  const setItemClassName = (item) => {
+    if (selectedRows.includes(item.id)) {
+      return 'list-item selected-list-item';
+    }
+    if (hiddenEvents.includes(item.id)) {
+      return 'list-item hidden-event';
+    }
+    if (hiddenEvents.includes(item.id) && selectedRows.includes(item.id)) {
+      return 'list-item selected-list-item';
+    }   
+    if (item.dateTime < Date.now()) {
+      return 'list-item past-event';
+    }
+    if (item.dateTime < Date.now() && selectedRows.includes(item.id)) {
+      return 'list-item selected-list-item';
+    }
+    return 'list-item';
+  }
+
+  const handlerEventHide = () => {
+    dispatch(setHiddenEvents(selectedRows));
+    setSelectedRows([]);
+  }; 
+  
+  const getDivider = (data) => {
+    return <Divider className="list-item-divider" orientation="left">{data}</Divider>  
+  };
+  
+  const getMonthAndYearDivider = (data, index) => {
+    const currentDate = dateByMonthAndYearParse(data[index].dateTime);
+    const prevIdx = data[index - 1];
+
+    if (prevIdx) {      
+      const previousDate = dateByMonthAndYearParse(data[index - 1].dateTime);
+
+      if (previousDate !== currentDate) {
+        return getDivider(currentDate);
+      }
+    } else {
+      return getDivider(currentDate);
+    }
+  }
+  
   return (
-    <List
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={item => (
-        <List.Item>
-          <div className="tag-wrapper">
-            <Tag className="list-item-tag" color={setTagColor(item.type)}>
-              {item.type
-                .toUpperCase()
-                .split('')
-                .map(i => (i === '-' ? ' ' : i))
-                .join('')}
-            </Tag>
-          </div>
-          <List.Item.Meta
-            title={<a href="https://ant.design">{item.topic}</a>}
-            description={`${item.date}, ${item.time}`}
-          />
-          {item.organizer ? (
-            <div className="item-organizer">
-              <span className="item-organizer-label">organizer:</span>
-              {GithubUserLink(item.organizer)}
-            </div>
+    <div className="list-item-wrapper">
+      <div className="hide-button-wrapper">        
+          {selectedRows.length ? (
+            <EventHideButton handlerEventHide={handlerEventHide} />
           ) : (
             ''
           )}
-        </List.Item>
-      )}
-    />
+      </div> 
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item, index) => (
+         <> 
+          {getMonthAndYearDivider(data, index)}
+          <List.Item 
+            className={setItemClassName(item)}
+            onClickCapture={(event) => handleClickCapture(event, item.id)}
+            onDoubleClick={() => showTaskInfo(item.id)}>
+            <div className="button-wrapper">
+              {hiddenEvents.includes(item.id)
+              ? <Button
+                  type="text"
+                  className="show-event-button list-item-button"
+                  onClick={() => handlerEventShow(item.id)}
+                >      
+                  <EyeOutlined />
+                </Button>
+              : ''}  
+            </div>           
+            <div className="tag-wrapper">
+              <Tag className="list-item-tag" color={setTagColor(item.type)}>
+                {item.type
+                  .toUpperCase()
+                  .split('')
+                  .map(i => (i === '-' ? ' ' : i))
+                  .join('')}
+              </Tag>
+            </div>
+            <List.Item.Meta
+              title={
+                <Button
+                  type="link"
+                  className="info-event-button"
+                  onClick={() => showTaskInfo(item.id)}
+                  style={{padding: '0'}}
+                >
+                  {item.topic}
+                </Button>}
+              description={`${dateByMonthAndDayParse(item.dateTime)}, ${item.time}`}
+            />
+            {item.organizer ? (
+              <div className="item-organizer">
+                <span className="item-organizer-label">organizer:</span>
+                {GithubUserLink(item.organizer)}
+              </div>
+            ) : (
+              ''
+            )}
+          </List.Item>   
+        </>         
+        )}
+      />
+    </div>  
   );
 };
 
