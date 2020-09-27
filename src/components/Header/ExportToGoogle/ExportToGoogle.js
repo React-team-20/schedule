636c28ce5d2bb304f922786ee7f4548f00890f44
Anton <akrayushkin@yandex.ru>
2020-09-27 16:57:52 +0300

    refactor: apply prettier and eslint

--- src/components/Header/ExportToGoogle/ExportToGoogle.js
+++ src/components/Header/ExportToGoogle/ExportToGoogle.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {ExportOutlined} from '@ant-design/icons';
@@ -11,44 +10,46 @@ const ExportToGoogle = () => {
   const data = useSelector(state => state.events);
   const {events} = data;
 
-  const eventsList = events.map((event) => {
-    return {   
-      'summary': event.topic,
-      'location': (event.place.address) ? event.place.address : '',
-      'description': event.description,
-      'start': {
-        'dateTime': moment(event.dateTime).format(),
-        'timeZone': 'Europe/Minsk'
+  const eventsList = events.map(event => {
+    return {
+      summary: event.topic,
+      location: event.place.address ? event.place.address : '',
+      description: event.description,
+      start: {
+        dateTime: moment(event.dateTime).format(),
+        timeZone: 'Europe/Minsk',
       },
-      'end': {
-        'dateTime': moment(event.dateTime).format(),
-        'timeZone': 'Europe/Minsk'
+      end: {
+        dateTime: moment(event.dateTime).format(),
+        timeZone: 'Europe/Minsk',
       },
-      'reminders': {
-        'useDefault': false,
-        'overrides': [
-          {'method': 'email', 'minutes': 24 * 60},
-          {'method': 'popup', 'minutes': 10}
-        ]
+      reminders: {
+        useDefault: false,
+        overrides: [
+          {method: 'email', minutes: 24 * 60},
+          {method: 'popup', minutes: 10},
+        ],
       },
-      'source': {
-        'url': (event.descriptionUrl) ? event.descriptionUrl : 'https://google.com',
-        'title': 'Description url'
-      }
-    }
+      source: {
+        url: event.descriptionUrl ? event.descriptionUrl : 'https://google.com',
+        title: 'Description url',
+      },
+    };
   });
 
-  const getListEventsFromCalendar = () => {    
-    return gapi.client.calendar.events.list({
-      'calendarId': 'primary',
-      'showDeleted': false,
-      'timeMin': (new Date()).toISOString(),
-      'singleEvents': true,
-      'orderBy': 'startTime',          
-      'colorRgbFormat': true
-    }).then(response => {      
-      return response.result.items;
-    });
+  const getListEventsFromCalendar = () => {
+    return gapi.client.calendar.events
+      .list({
+        calendarId: 'primary',
+        showDeleted: false,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: 'startTime',
+        colorRgbFormat: true,
+      })
+      .then(response => {
+        return response.result.items;
+      });
   };
 
   const handleClick = () => {
@@ -58,55 +59,59 @@ const ExportToGoogle = () => {
         clientId: CALENDAR_API.CLIENT_ID,
         discoveryDocs: CALENDAR_API.DISCOVERY_DOCS,
         scope: CALENDAR_API.SCOPES,
-      })
-
-      gapi.auth2.getAuthInstance().signIn()
-      .then(async () => {
-        const calandarEvents = await getListEventsFromCalendar();      
-        const lastIndex = eventsList.length - 1;
-
-        eventsList.map((event, index) => {         
-          const eventExisting = calandarEvents.filter((item) => item.start.dateTime === event.start.dateTime);
-         
-          if (eventExisting.length > 0) {
-            const eventId = eventExisting[0].id;
-            
-            const request = gapi.client.calendar.events.update({
-              'calendarId': 'primary',
-              'eventId': eventId,
-              'resource': event
-            });
-            
-            request.execute(event => {
-              if (lastIndex === index) {
-                window.open(event.htmlLink)
-              }            
-            })
-          }
-
-          if (eventExisting.length === 0) {
-            const request = gapi.client.calendar.events.insert({
-              'calendarId': 'primary',
-              'resource': event,
-            });
-
-            request.execute(event => {           
-              if (lastIndex === index) {
-                window.open(event.htmlLink)
-              }            
-            });
-          }
-        });
       });
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(async () => {
+          const calandarEvents = await getListEventsFromCalendar();
+          const lastIndex = eventsList.length - 1;
+
+          eventsList.map((event, index) => {
+            const eventExisting = calandarEvents.filter(
+              item => item.start.dateTime === event.start.dateTime
+            );
+
+            if (eventExisting.length > 0) {
+              const eventId = eventExisting[0].id;
+
+              const request = gapi.client.calendar.events.update({
+                calendarId: 'primary',
+                eventId: eventId,
+                resource: event,
+              });
+
+              request.execute(event => {
+                if (lastIndex === index) {
+                  window.open(event.htmlLink);
+                }
+              });
+            }
+
+            if (eventExisting.length === 0) {
+              const request = gapi.client.calendar.events.insert({
+                calendarId: 'primary',
+                resource: event,
+              });
+
+              request.execute(event => {
+                if (lastIndex === index) {
+                  window.open(event.htmlLink);
+                }
+              });
+            }
+          });
+        });
     });
-  }
+  };
 
   return (
-    <Tooltip title='export to Google calendar'>
-      <Button className='button-center-icon button-export' onClick={handleClick}>
+    <Tooltip title="export to Google calendar">
+      <Button className="button-center-icon button-export" onClick={handleClick}>
         <ExportOutlined />
       </Button>
-    </Tooltip>  
+    </Tooltip>
   );
 };
 
